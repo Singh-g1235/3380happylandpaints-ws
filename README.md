6f5404c0b13d6d38c4f8ad69d01f6255bdb2ed1d
Lovepreet Singh <singhl22@student.douglascollge.ca>
2021-04-10 19:53:36 -0700

    added git url

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# git hub url
 
+git@github.com:Singh-g1235/3380happylandpaints-ws.git
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken');
 
-const url='mongodb+srv://happylandpaints:happy@happylandpaints.su0wq.mongodb.net/paints';
+//const url='mongodb+srv://happylandpaints:happy@happylandpaints.su0wq.mongodb.net/paints';
+const url='mongodb://localhost:27017/paints';
 
 const app = express();
 const port = 8000
